153f5bbcc481b2b57d5698bfc0a5b1591a4efc6b
M Alexander Moroney <alexmoroney@gmail.com>
2022-08-27 16:58:37 -0500

    L291 -Introducing React Router

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>click me</button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/page2" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
