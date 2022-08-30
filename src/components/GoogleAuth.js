6fa1aaf913f6773cb737a7aadd93629f28c61776
M Alexander Moroney <alexmoroney@gmail.com>
2022-08-29 22:44:28 -0500

    L311 - Updating Auth State

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -14,9 +14,19 @@ class GoogleAuth extends React.Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+          // The .listen function can work as an event listener which calls a callback funciton whenever the user authentication status changes
+          // We can use this to update state and therefore rerender the components on screen without refreshing the page using the .listen()
         });
     });
   }
+
+  onAuthChange = () => {
+    // This function monitors the user's current authentication status. when paired with the isSignedIn.listen() method...
+    // We can determnine if the the user is currently signed in or not.
+    // *NB* note that we've defind onAuthChange using an arrow function so that its context is bound to the object (or function) that it's being called from.
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
