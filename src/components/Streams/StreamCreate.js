e39cd6c58aa80ffb2b892cefdd76e1df404be12c
M Alexander Moroney <alexmoroney@gmail.com>
2022-08-31 23:24:24 -0500

    L328 - Automatically Handling Events

--- src/components/Streams/StreamCreate.js
+++ src/components/Streams/StreamCreate.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    // The renderInput helper method is used to pass JSX to the Field element from the redux-form library.
+    // We can destructure the "input" property from formProps.input and call it as a prop within the <input /> element as shown below.
+    return <input {...input} />;
+    // In most cases when using redux-form we'll return an input element.
+  }
   render() {
     console.log(this.props);
     return (
       <form>
-        <Field name="title" />
-        <Field name="description" />
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
       </form>
     );
   }
@@ -16,4 +22,4 @@ export default reduxForm({
   form: "streamCreate",
 })(StreamCreate);
 // The reduxForm() function has a similar syntax to the connect() function
-// Unlike the connect() funciton which receives multiple arguments, reduxForm() receives a sing object and we put different configuration within this object
+// Unlike the connect() funciton which receives multiple arguments, reduxForm() receives a single object and we put different configuration within this object
