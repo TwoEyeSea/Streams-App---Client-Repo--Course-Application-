99d41adcca9844de5038d1717de49d529145b627
M Alexander Moroney <alexmoroney@gmail.com>
2022-08-31 22:32:25 -0500

    L326 - Connecting Redux Form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
+// To maintain clarity of our reducers we can import the reducer object from redux-form with additional syntax
+// importing the reducer "as formReducer" and calling this object by calling formReducer as shown below.
 import authReducer from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
+  form: formReducer,
 });
