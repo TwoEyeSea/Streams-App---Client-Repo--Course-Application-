008dbfed3e88df81f83e235e465662e9dcf00f60
M Alexander Moroney <alexmoroney@gmail.com>
2022-09-01 19:42:36 -0500

    L334 - Highlighting Errored Fields

--- src/components/Streams/StreamCreate.js
+++ src/components/Streams/StreamCreate.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 class StreamCreate extends React.Component {
-  renderInput({ input, label, meta }) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
     // The renderInput helper method is used to pass JSX to the Field element from the redux-form library.
     // We can destructure the "input" property from formProps.input and call it as a prop within the <input /> element as shown below.
+
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         {/* The label prop is destructured within the renderInput function and placed within the <lable></lable> element 
         This will display the string for each label prop as defined within the <Field/> elements below */}
-        <input {...input} />
-        <div>{meta.error}</div>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
     // In most cases when using redux-form we'll return an input element.
-  }
+  };
 
   onSubmit(formValues) {
     console.log(formValues);
@@ -22,7 +35,8 @@ class StreamCreate extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+        {/**NOTE that semantic ui will hide any error messages unless we add the className "error"*/}
         {/* onSubmit={} is the name of the prop that we're pasisng onto the <form> element.
           this.props.handleSubmit() is a callback function provided to our component by redux-form.
           Now when the form is submitted the handleSubmit() callback function will receive the event object and automatically call event.preventDefault */}
