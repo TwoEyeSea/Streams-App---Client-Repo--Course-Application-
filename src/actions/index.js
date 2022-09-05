a6aca76334456b8e323e4dbe3b381c1fc4b89fa8
M Alexander Moroney <alexmoroney@gmail.com>
2022-09-05 18:12:40 -0500

    L341 - Bulk Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import { SIGN_IN, SIGN_OUT, FETCH_STREAMS, DELETE_STREAM, CREATE_STREAM, EDIT_STREAM, FETCH_STREAM } from "./types";
+
 import streams from "../apis/streams";
 // imported the variables from the ./types dir to reduce the chances of making transcription typos when defining types.
 
@@ -22,4 +23,30 @@ export const createStream = (formValues) => async (dispatch) => {
   dispatch({ type: CREATE_STREAM, payload: response.data });
   // we give the payload a value of response.data because we only care about the information from the request
   // In this case, response.data will contain information on the title and description of a stream.
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (streamId) => async (dispatch) => {
+  const response = await streams.get(`/streams/:${streamId}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStreams = (streamId, formValues) => async (dispatch) => {
+  const response = await streams.put(`/streams/:${streamId}`, formValues);
+  // To modify a record we need the ID of individual stream we want to modify/update and the actual updates we intend on making to the stream.
+  // We pass the streamId and the formValues terms as arges to the action creator.
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStreams = (streamId) => async (dispatch) => {
+  await streams.delete(`/streams/:${streamId}`);
+
+  dispatch({ type: DELETE_STREAM, payload: streamId });
 };
