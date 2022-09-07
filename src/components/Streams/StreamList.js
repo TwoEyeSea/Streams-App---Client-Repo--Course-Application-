f91fd401dd450eb0412cf5cdf59b1e3163728cb2
M Alexander Moroney <alexmoroney@gmail.com>
2022-09-06 19:39:07 -0500

    L350 - Conditionally Showing Edit and Delete

--- src/components/Streams/StreamList.js
+++ src/components/Streams/StreamList.js
@@ -7,11 +7,26 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin = (stream) => {
+    // Helper function to provide the logic necessary to determine if the delete and edit buttons should be displayed for a given stream.
+    // If the stream's userId state === currentUserId state we'll render the administrative buttons.
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button red">Delete</button>
+        </div>
+      );
+    }
+  };
+
   renderList() {
     console.log(this.props.streams);
     return this.props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
+          {/* Helper function containing logic to determine if admin buttons should be rendered for a given stream (based on userId) */}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -33,7 +48,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
