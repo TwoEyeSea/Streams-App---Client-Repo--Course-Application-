23e5c6261ed84a217463507791bbd108a7d196e7
M Alexander Moroney <alexmoroney@gmail.com>
2022-09-07 21:16:12 -0500

    annotation

--- src/components/Streams/StreamEdit.js
+++ src/components/Streams/StreamEdit.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions/index";
+//importing the fetchStream action creator because this application is making use of URL Navigation
+// This StreamEdit component needs to have access to the data by itself to be function (even in the event of the user loading this component first)
+// Otherwise users will always need to load the StreamList component first to fetch the required data.
+// Now the StreamEdit Component will fetchn the required data necessary to edit this stream via componentDidMount.
 
 class StreamEdit extends React.Component {
   componentDidMount() {
