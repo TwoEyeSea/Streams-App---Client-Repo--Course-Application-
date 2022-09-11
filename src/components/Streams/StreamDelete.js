ffea697d6acea22c66f12311a548d13330229fd7
M Alexander Moroney <alexmoroney@gmail.com>
2022-09-11 01:10:48 -0500

    L374 - Making the Modal Reusable.

--- src/components/Streams/StreamDelete.js
+++ src/components/Streams/StreamDelete.js
@@ -2,10 +2,19 @@ import React from "react";
 import Modal from "../modal";
 
 const StreamDelete = () => {
+  const actions = (
+    <div>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button"> Cancel</button>
+    </div>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal title="Delete Stream" content="Are you sure you want to delete this stream?" actions={actions} />
+      {/* To make the modal more reusable we're going to pass a prop called title and add a string to that prop that will be displayed.
+      Within the modal itself we'll call props.title to display any given title we pass to the modal in the future. */}
     </div>
   );
 };
--- src/components/modal.js
+++ src/components/modal.js
@@ -6,13 +6,10 @@ const Modal = (props) => {
   return ReactDOM.createPortal(
     <div onClick={() => history.push("/")} className="ui dimmer modals visible active">
       {/* semantic ui class names */}
-      <div onClick={(e) => e.stopPropagation} className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
-        <div className="content">Are you sure you want to delete this stream?</div>
-        <div className="actions">
-          <div className="ui approve button primary">Delete Stream</div>
-          <div className="ui cancel button">Cancel</div>
-        </div>
+      <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector("#modal")
