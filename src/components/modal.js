be895b4546f200df70d8b48aa1f41aa45880227f
M Alexander Moroney <alexmoroney@gmail.com>
2022-09-11 00:51:42 -0500

    L373 - Hiding a Modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
+    <div onClick={() => history.push("/")} className="ui dimmer modals visible active">
       {/* semantic ui class names */}
-      <div className="ui standard modal visible active">reqreqftrqfeq</div>
+      <div onClick={(e) => e.stopPropagation} className="ui standard modal visible active">
+        <div className="header">Delete Stream</div>
+        <div className="content">Are you sure you want to delete this stream?</div>
+        <div className="actions">
+          <div className="ui approve button primary">Delete Stream</div>
+          <div className="ui cancel button">Cancel</div>
+        </div>
+      </div>
     </div>,
     document.querySelector("#modal")
   );
