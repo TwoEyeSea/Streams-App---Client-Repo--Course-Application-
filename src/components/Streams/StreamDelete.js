e7452a5d631f1e7a3f800ec17728ae11baf61881
M Alexander Moroney <alexmoroney@gmail.com>
2022-09-11 13:46:12 -0500

    L376 - onDismiss From the Parent

--- src/components/Streams/StreamDelete.js
+++ src/components/Streams/StreamDelete.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "../modal";
+import history from "../../history";
 
 const StreamDelete = () => {
   const actions = (
@@ -12,9 +13,17 @@ const StreamDelete = () => {
   return (
     <div>
       StreamDelete
-      <Modal title="Delete Streams" content="Are you sure you want to delete this stream?" actions={actions} />
-      {/* To make the modal more reusable we're going to pass a prop called title and add a string to that prop that will be displayed.
-      Within the modal itself we'll call props.title to display any given title we pass to the modal in the future. */}
+      <Modal
+        // To make the modal more reusable we're going to pass a prop called title and add a string to that prop that will be displayed.
+        // Within the modal itself we'll call props.title to display any given title we pass to the modal in the future.
+        title="Delete Streams"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+        // We've defined multiple props on the modal from the parent to ensure that the modal can be imported and reused by multiple parent objects
+        // by follow the same convention.
+        // onDismiss is standard volcabulary for parental programmatic navigation
+      />
     </div>
   );
 };
