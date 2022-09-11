27debe62390799b8225fc43ef24f780fb09d8f1f
M Alexander Moroney <alexmoroney@gmail.com>
2022-09-11 13:37:11 -0500

    L375 - React Fragments

--- src/components/Streams/StreamDelete.js
+++ src/components/Streams/StreamDelete.js
@@ -3,16 +3,16 @@ import Modal from "../modal";
 
 const StreamDelete = () => {
   const actions = (
-    <div>
+    <React.Fragment>
       <button className="ui button negative">Delete</button>
       <button className="ui button"> Cancel</button>
-    </div>
+    </React.Fragment>
   );
 
   return (
     <div>
       StreamDelete
-      <Modal title="Delete Stream" content="Are you sure you want to delete this stream?" actions={actions} />
+      <Modal title="Delete Streams" content="Are you sure you want to delete this stream?" actions={actions} />
       {/* To make the modal more reusable we're going to pass a prop called title and add a string to that prop that will be displayed.
       Within the modal itself we'll call props.title to display any given title we pass to the modal in the future. */}
     </div>
